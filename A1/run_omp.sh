cbeee2d2f77ca763b10ec9cbc117791e24ec57e1
Ye <943157377@qq.com>
2021-09-20 12:34:02 +0800

    update test case

--- A1/run_omp.sh
+++ A1/run_omp.sh
@@ -7,7 +7,7 @@ echo "RUNNING OMP IMPLEMENTATION..."
 
 for filename in test_cases/*.in; do
     echo "RUNNING $filename"
-    for ((counter = 0; counter < 1; counter ++)) do
+    for ((counter = 0; counter < 3; counter ++)) do
         echo "run $counter"
         ./goi_omp.out "$filename" deathToll.out $1
     done
